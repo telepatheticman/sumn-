23b9d029e338cd64f396a4365487de51ef79cf09
telepatheticman <telepatheticman13@gmail.com>
2016-03-23 18:13:17 -0500

    Init Commit

---
+++ FRC/src/apps/Config.java
@@ -0,0 +1,12 @@
+package apps;
+import java.io.*;
+// http://www3.ntu.edu.sg/home/ehchua/programming/java/j4a_gui.html
+
+public class Config {
+	public static void main(String args[]){
+		
+	}
+	public void GUI(){
+		
+	}
+}
